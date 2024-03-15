c8f2fd6968ba7bb0f73a3e1ab5d37bce9c8f44c4
Robin Martinez <romartin@k0r3p4.42mulhouse.fr>
2024-03-15 16:57:56 +0100

    cpp07fini

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -10,33 +10,43 @@ class Array {
         T* array;
         unsigned int size;
     public:
-        Array(void): array (new T()), size (0) {};
+        Array(void): array (new T[0]), size (0) {};
         Array(unsigned int nb): array (new T[nb]), size (nb) {};
-        Array (const Array& src): array (new T[src.size]) {
-        int i = 0;
+        Array (const Array& src): array (new T[src.size]), size(src.size) {
+        unsigned int i = 0;
         while (i < size)
         {
             array[i] = src.array[i];
             i++;
         }
        };
+       T& operator[] (unsigned int index) const {
+            if (index >= size) {
+                throw Out_Of_Range();
+            }
+            return (array[index]);
+       }
        Array& operator=(const Array &src) 
        {
             if (this != &src)
             {
                 delete [] array;
-                array = new T[src.size()];
+                array = new T[size];
                 size = src.size;
-                int i = 0;
+                unsigned int i = 0;
                 while (i < size)
                 {
                     array[i] = src.array[i];
                     i++;
                 }
             }
+            return *this;
        };
-       ~Array(void){ delete [] array;};
-    
+      ~Array(void){ delete [] array;};
+    class Out_Of_Range: public std::exception {
+        public:
+            virtual const char* what() const throw() { return "index out of range";}
+    };
 };
 
 #endif
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,15 +6,34 @@
 int main( void )
 {
     std::cout << "-----------------------------------------------------" << std::endl;
-
     Array< float > test;
 
+    Array< int > intVoid ( void );
     Array< int > intArray( SIZE );
-    Array< int > intArray2( SIZE - 5 );
+    Array< int > intArray2( SIZE );
+    Array< int > intArray3( 1 );
 
-    //std::cout << intArray << std::endl;
-
-
+    for (int i = 0; i < SIZE; ++i) {
+        intArray[i] = i;
+    }
+    intArray2 = intArray;
+    intArray3[0] = 258;
+    for (int i = 0; i < SIZE; ++i) {
+        std::cout << intArray[i] << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < SIZE; ++i) {
+        std::cout << intArray2[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << intArray3[0] << std::endl;
+    
+    try {
+        std::cout << intArray[0] << std::endl;
+        std::cout << intArray[100] << std::endl;
+    } catch ( Array< int >::Out_Of_Range& e ) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
     std::cout << "-----------------------------------------------------" << std::endl;
 
     return ( 0 );

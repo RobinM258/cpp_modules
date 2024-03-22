67e96dfc72956aec08b79f924aeaebb7bb02b3e1
Robin Martinez <romartin@k0r1p5.42mulhouse.fr>
2024-03-22 15:35:32 +0100

    cpp08

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,27 @@
+#ifndef MUTANT
+#define MUTANT
+
+#include <iostream>
+#include <stack>
+#include <list>
+
+template<typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+    public:
+        MutantStack(void) : std::stack<T, Container>() {}
+        ~MutantStack(void) {}
+
+        typedef typename Container::iterator iterator;
+
+        iterator begin(void)
+        {
+            return this->c.begin();
+        }
+        iterator end(void)
+        {
+            return this->c.end();
+        }
+};
+
+#endif
---
+++ CPP08/ex02/main.cpp
@@ -0,0 +1,52 @@
+#include "MutantStack.hpp"
+
+int main()
+{
+    {
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
+        std::cout << mstack.top() << std::endl;
+        mstack.pop();
+        std::cout << mstack.size() << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::stack<int> s(mstack);
+        std::cout << s.size() << std::endl;
+    }
+    std::cout << "--------------------------------------------------" << std::endl;
+    {
+        std::list<int> mlist;
+        mlist.push_back(5);
+        mlist.push_back(17);
+        std::cout << mlist.back() << std::endl;
+        mlist.pop_back();
+        std::cout << mlist.size() << std::endl;
+        mlist.push_back(3);
+        mlist.push_back(5);
+        mlist.push_back(737);
+        mlist.push_back(0);
+        std::list<int>::iterator il = mlist.begin();
+        std::list<int>::iterator ile = mlist.end();
+        ++il;
+        --il;
+        while (il != ile)
+        {
+            std::cout << *il << std::endl;
+            ++il;
+        }
+
+    }
+    return 0;
+}

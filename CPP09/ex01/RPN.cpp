e11b3859235ed4c0c33d6d19d45d437fdc546e8b
robin <darovi.martinez@gmail.com>
2025-01-16 19:27:05 +0000

    ex01 normalement fix

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -21,18 +21,12 @@ void Rpn::calculate(std::string tab)
             ;
         else if (std::isdigit(tab[i]))
             value.push(tab[i] - '0');
-        else if (value.size() != 2)
-        {
-            std::cout << "Error" << std::endl;
-            return ;
-        }
-        else
+        else if (value.size() >= 2)
         {
             int tmp = value.top();
             value.pop();
             switch (tab[i])
             {
-                
                 case '+':
                     tmp = value.top() + tmp;
                     break;
@@ -49,11 +43,20 @@ void Rpn::calculate(std::string tab)
                     std::cout << "Error" << std::endl;
                     break;
             }
-            
             value.pop();
             value.push(tmp);
         }
+        else
+        {
+            std::cout << "Error" << std::endl;
+            return;
+        }
         i++;
+    }
+    if (value.size() != 1)
+    {
+        std::cout << "Error " <<  std::endl;
+        return;
     }
     std::cout << value.top() << std::endl;
     
